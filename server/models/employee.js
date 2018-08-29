cc3d3c773934bec7c1c2f70287068613138181c8
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-08-29 20:45:59 +0300

    set default value for new employee model

--- server/models/employee.js
+++ server/models/employee.js
@@ -1,8 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Employee = sequelize.define('Employee', {
-    name: DataTypes.STRING,
-    active: DataTypes.BOOLEAN
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    }
   }, {});
   Employee.associate = function(models) {
     // associations can be defined here
